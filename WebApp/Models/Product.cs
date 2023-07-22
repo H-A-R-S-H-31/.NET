3394dba8ed8f6ba79aa63eb9d2fc80fd543bbfe0
dac <dac@dypm.org>
2023-07-22 19:27:00 +0530

    Product_CRUD

---
+++ WebApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int qty { get; set; }
+    }
+}
