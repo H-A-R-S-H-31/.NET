0abc0e6ecc36c1f8fb4a68d1f69f3d87980589c6
dac <dac@dypm.org>
2023-07-25 20:59:12 +0530

    Employee

---
+++ WebAppEmp/WebApp/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary{get; set;}
+
+    }
+}
